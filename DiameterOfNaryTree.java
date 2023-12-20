32098deb5fb664c4e10e9849af0556edd9d4a6a0
henok3878 <henokwondimu3878@gmail.com>
2023-12-20 17:45:07 +0300

    Solve diameter of n-arr tree

---
+++ DiameterOfNaryTree.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int diameter(Node root) {
+        if (root == null || root.children.size() == 0) {
+            return 0;
+        }
+
+        int[] maxDiameter = new int[1];
+        diameterHelper(root, maxDiameter);
+        return maxDiameter[0];
+    }
+
+    private int diameterHelper(Node root, int[] maxDiameter) {
+        if (root.children.size() == 0) {
+            return 0;
+        }
+
+        // Setting below maximums to -1 helps in the case if there is only one child
+        // node of this root node.
+        int maxHeight1 = -1;
+        int maxHeight2 = -1;
+
+        for (Node child : root.children) {
+            int childHeight = diameterHelper(child, maxDiameter);
+            if (childHeight > maxHeight1) {
+                maxHeight2 = maxHeight1;
+                maxHeight1 = childHeight;
+            } else if (childHeight > maxHeight2) {
+                maxHeight2 = childHeight;
+            }
+        }
+
+        maxDiameter[0] = Math.max(maxDiameter[0], maxHeight1 + maxHeight2 + 2);
+        return maxHeight1 + 1;
+    }
+}
